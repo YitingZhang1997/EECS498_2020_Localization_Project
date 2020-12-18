0e33153aec517d0ed66e6b37274f644277745913
hujie <huijiezh@umich.edu>
2020-12-19 01:49:02 +0800

    install.sh running time

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ echo the version of openravepy is 0.9.0
 echo the version of numpy is 1.13.3
 echo the version of matplotlib is 2.2.5
 echo no other packages need.
+echo demo.py total runtime would be 8-10 min
