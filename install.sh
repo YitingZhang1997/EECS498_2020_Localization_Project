239886e18417e2bef6cfaf640298dc7b75f4c2d7
hujie <huijiezh@umich.edu>
2020-12-19 01:35:37 +0800

    install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+echo the project is using python2.7
+echo the version of openravepy is 0.9.0
+echo the version of numpy is 1.13.3
+echo the version of matplotlib is 2.2.5
+echo no other packages need.
